52150cafe9a88a228ea52178f04401a275fd017c
OscarTekomahua <20223tn042@utez.edu.mx>
2023-08-22 11:49:29 -0600

    Agregue algunos jsp a los filtros y cree el de usuario :)

--- src/main/java/mx/edu/utez/sigebi/utils/FiltroSessionUser.java
+++ src/main/java/mx/edu/utez/sigebi/utils/FiltroSessionUser.java
@@ -6,54 +6,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
 @WebFilter(urlPatterns = {
-        "/admin.jsp",
-        "/administrarStockAdmin.jsp",
-        "/categoriasAdmin.jsp",
-        "/editorialesAdmin.jsp",
-        "/salasAdmin.jsp",
-        "/formulariocategoriaAdmin.jsp",
-        "/formularioeditorialAdmin.jsp",
-        "/usuariosadmin.jsp",
-        "/readEditorialAdmin",
-        "/createEditorialAdmin",
-        "/createCategoriaAdmin",
-        "/readCategoriaAdmin",
-        "/formularioeditorialmodAdmin.jsp",
-        "/nuevousuario.jsp",
-        "/agregarlibroAdmin.jsp"
+        "/inicio.jsp",
+        "/salasuser.jsp",
+        "/vistalibro.jsp",
+        "/solicitarsala.jsp",
+        "/catalogoLibros.jsp"
 })
-public class FiltroSesionAdmin implements Filter {
-
+public class FiltroSessionUser implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
+    public void doFilter(ServletRequest req,
+                         ServletResponse resp,
                          FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        HttpServletResponse httpResponse = (HttpServletResponse) resp;
 
         // Get the HttpSession from the request
         HttpSession session = httpRequest.getSession(false);
 
         // Check if the user is an admin(assuming isAdmin is a boolean session attribute)
-        boolean isAdmin = false;
+        boolean isUser = false;
         if(session != null){
-            System.out.println(session.getAttribute("tipoUsuario"));
             if (session.getAttribute("tipoUsuario") != null) {
-                isAdmin = session.getAttribute("tipoUsuario").equals("Administrador");
+                isUser = session.getAttribute("tipoUsuario").equals("Usuario UTEZ");
             }
         }
 
-        if (isAdmin) {
+        if (isUser) {
             // If the user is an admin, allow access to the Servlet
-            chain.doFilter(request, response);
+            chain.doFilter(req, resp);
         } else {
             // If the user is not an admin, redirect them to a restricted access page
             httpResponse.sendRedirect("failedAccess.jsp");
@@ -64,5 +51,4 @@ public class FiltroSesionAdmin implements Filter {
     public void destroy() {
         // Cleanup code if needed (optional)
     }
-
 }
